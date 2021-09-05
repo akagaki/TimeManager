21b03e336096c6655c18dfa01cc8b4b10938cc16
akagaki <taka.a.0401@gmail.com>
2021-09-05 16:25:31 +0900

    カレンダーのデザインを修正

--- src/resources/js/components/calendarItem.js
+++ src/resources/js/components/calendarItem.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 class CalendarItem extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+        date: new Date(),
+    }
+  };
   render() {
     return (
       <div className="my-4">
         Calendar
-        <Calendar locale="ja-JP" value={new Date()} />
+        <Calendar locale="ja-JP" value={this.state.date} />
       </div>
     );
   }
