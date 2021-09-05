3070eeb9f0a0b6e8dd7b006afd01ad4fae0c85b3
akagaki <taka.a.0401@gmail.com>
2021-09-06 01:53:54 +0900

    configファイルのAPP_NAMEを修正

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -62,7 +62,7 @@
     <nav class="navbar navbar-expand-md navbar-light bg-light shadow fixed-top">
         <div class="container">
             <a class="navbar-brand" href="{{ url('/') }}">
-                {{ TimeManager }}
+                 {{ config('app.name') }}
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                 <span class="navbar-toggler-icon"></span>
